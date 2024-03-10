68addac3fee4c57bfb64d080cd3b41fccaddadaf
Ricardo Alvarez <rasoftdev@outlook.com>
2024-03-10 15:58:20 -0500

    [ADD] Add base routes Auth

---
+++ src/routes/index.js
@@ -0,0 +1,28 @@
+import Auth from "../views/Auth/Auth";
+import Register from "../views/Auth/Register";
+import RecoverPassword from "../views/Auth/RecoverPassword";
+
+const routes = [
+    {
+        path: "/",
+        icon: "",
+        layout: "/auth",
+        component: Auth,
+        invisible: true
+    },
+    {
+        path: "/register",
+        icon: "",
+        layout: "/auth",
+        component: Register,
+        invisible: true
+    },
+    {
+        path: "/recover-password",
+        icon: "",
+        layout: "/auth",
+        component: RecoverPassword,
+        invisible: true
+    }
+];
+export default routes;
---
+++ src/views/Auth/RecoverPassword.jsx
@@ -0,0 +1,6 @@
+const RecoverPassword = () => {
+    return (
+        <></>
+    )
+}
+export default RecoverPassword;
