514f8fb95aa08f850c857f58b8d7f9cbc47feb15
Ricardo Alvarez <rasoftdev@outlook.com>
2024-03-10 18:11:44 -0500

    [ADD] Add base context auth and base structure app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
 import React, { useState } from 'react'
-import { AuthProvider } from "./conext/AuthContext.js";
-import store from "./store/store.js";
-import Auth from "./layouts/Auth.jsx";
 
 function App() {
     return (
         <>
             <div>
-                <BrowserRouter>
-                    <AuthProvider store={store}>
-                        <switch>
-                            <Route path="/auth" render={(props) => <Auth {...props}/>} />
-                            <Redirect from="*" to="/auth/" />
-                        </switch>
-                    </AuthProvider>
-                </BrowserRouter>
             </div>
         </>
     )
