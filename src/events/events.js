68c8c96b2bee6f402f2d0246e882f4960125805e
Ricardo Alvarez <rasoftdev@outlook.com>
2024-03-10 15:12:15 -0500

    [ADD] Add base components and structure app

---
+++ src/events/events.js
@@ -0,0 +1,14 @@
+const subscribe = (eventName, listener) => {
+    document.addEventListener(eventName, listener);
+}
+
+const unsubscribe = (eventName, listener) => {
+    document.removeEventListener(eventName, listener);
+}
+
+const publish = (eventName, data) => {
+    const event = new CustomEvent(eventName, { detail: data });
+    document.dispatchEvent(event);
+}
+
+export { publish, subscribe, unsubscribe };
