4aa0757014829df75f4766e6e1b61cad8fd5774f
Ricardo Alvarez <rasoftdev@outlook.com>
2024-03-10 22:35:53 -0500

    [ADD] Add base project and routes

--- README.md
+++ README.md
@@ -62,7 +62,6 @@ Sigue estos pasos para instalar y configurar el proyecto en tu entorno local:
 ##Package
 - [react-router-dom](https://www.npmjs.com/package/react-router-dom)
 - [axios](https://www.npmjs.com/package/axios)
-- [react-bootstrap](https://www.npmjs.com/package/react-bootstrap)
 -  [reactstrap](https://reactstrap.github.io/?path=/story/home-installation--page)
 - [react-redux](https://www.npmjs.com/package/react-redux)
 ## Autores
--- src/layouts/Auth.jsx
+++ src/layouts/Auth.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import routes from "../routes/index.js";
+import { Container, Row, Col } from "react-bootstrap";
 import { LoadingProvider } from "../context/LoadingContext.js";
 
 const Auth = () => {
     const getRoutes = (routes) => {
         return routes.map((prop, key) => {
-            if (prop.collapse) {
-                return getRoutes(prop.views);
-            }
             if (prop.layout === "/auth") {
                 return (
                     <Route
                         path={`${prop.path}`}
-                        element={<prop.component />}
+                        element={<prop.component/>}
                         key={key}
                     />
                 );
@@ -25,14 +23,20 @@ const Auth = () => {
     console.log(getRoutes(routes));
     return (
         <>
-            <div>
-                AUTH - LAYOUT
-                <LoadingProvider>
-                    <Routes>
-                        {getRoutes(routes)}
-                    </Routes>
-                </LoadingProvider>
-            </div>
+            <Container fluid>
+                <Row>
+                    <Col lg="6" md="6" sm="12">
+                        <LoadingProvider>
+                            <Routes>
+                                {getRoutes(routes)}
+                            </Routes>
+                        </LoadingProvider>
+                    </Col>
+                    <Col lg="6" md="6" sm="12">
+                        Content
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
