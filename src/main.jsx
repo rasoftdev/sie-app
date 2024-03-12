71cef8fac2a042e98ede36c3cf38e4d14ad5c6ba
Ricardo Alvarez <rasoftdev@outlook.com>
2024-03-12 16:21:39 -0500

    [REMOVE] Remove redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './store/Store';
 import App from './App.jsx';
 import './styles/app.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
--- src/reducers/RootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import AuthReducer from './Auth/AuthReducers';
-
-const rootReducer = combineReducers({
-    auth: AuthReducer
-});
-
-export default rootReducer;
